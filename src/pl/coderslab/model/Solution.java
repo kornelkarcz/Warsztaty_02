a7cc19380c36d87b9cc04b82601a9f892c344b04
kornelkarcz <kornel.karcz@outlook.com>
2019-01-20 13:34:50 +0100

    Dodanie metod do klasy Solution

--- src/pl/coderslab/model/Solution.java
+++ src/pl/coderslab/model/Solution.java
@@ -1,5 +1,10 @@
 package pl.coderslab.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Solution {
@@ -70,5 +75,89 @@ public class Solution {
         this.exercise = exercise;
     }
 
+    public void saveToDB(Connection conn) throws SQLException {
+        if (this.id == 0) {
+            String sql = "INSERT INTO solution(created, updated, description, users_id, exercise_id) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement
+                    = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);//z automatu wyciaga pierwsza kolumne z tabeli
+            preparedStatement.setString(1, this.created);
+            preparedStatement.setString(2, this.updated);
+            preparedStatement.setString(3, this.description);
+            preparedStatement.setInt(4, this.user.getId());
+            preparedStatement.setInt(5, this.exercise.getId());
+            preparedStatement.executeUpdate();
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            if (rs.next()) {
+                this.id = rs.getInt(1);
+            }
+        } else {
+            String sql = "UPDATE solution SET created=?, updated=?, description=?, users_id=?, exercise_id=? where id = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, this.created);
+            preparedStatement.setString(2, this.updated);
+            preparedStatement.setString(3, this.description);
+            preparedStatement.setInt(4, this.user.getId());
+            preparedStatement.setInt(5, this.exercise.getId());
+            preparedStatement.setInt(6, this.id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    static public Solution loadSolutionById(Connection conn, int id) throws SQLException {
+        String sql = "SELECT * FROM solution where id=?";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) { //dajemy tutaj if a nie while tak jak zawsze bo potrzebujemy tylko wczytac 1 usera
+            Solution loadedSolution = new Solution();
+            loadedSolution.id = resultSet.getInt("id");
+            loadedSolution.created = resultSet.getString("created");
+            loadedSolution.updated = resultSet.getString("updated");
+            loadedSolution.description = resultSet.getString("description");
+            //loadedSolution.user = new User();
+
+            return loadedSolution;
+        }
+        return null;
+    }
+
+    static public ArrayList<Solution> loadAllSolutions(Connection conn) throws SQLException {
+        ArrayList<Solution> solutions = new ArrayList<Solution>();
+        String sql = "SELECT * FROM solution";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            Solution loadedSolution = new Solution();
+            loadedSolution.id = resultSet.getInt("id");
+            loadedSolution.created = resultSet.getString("created");
+            loadedSolution.updated = resultSet.getString("updated");
+            loadedSolution.description = resultSet.getString("description");
+            //loadedSolution.user = new User();
+            solutions.add(loadedSolution);
+        }
+        return solutions;
+    }
+
+    public void delete(Connection conn) throws SQLException {
+        if (this.id != 0) {
+            String sql = "DELETE FROM solution WHERE id=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, this.id);
+            preparedStatement.executeUpdate();
+            this.id = 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Solution{" +
+                "id=" + id +
+                ", created='" + created + '\'' +
+                ", updated='" + updated + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user + '\'' +
+                ", exercise=" + exercise + '\'' +
+                '}';
+    }
 
 }
